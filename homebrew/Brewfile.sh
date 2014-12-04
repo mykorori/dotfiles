b7c070379101501dee38f6b956051e0b20d03e6d
yukiyo <yuki@giamnote.net>
2014-12-04 14:38:23 +0900

    modify install.sh

---
+++ homebrew/Brewfile.sh
@@ -0,0 +1,132 @@
+# Refer to:
+# https://github.com/tcnksm/dotfiles/blob/master/homebrew/install.sh
+
+# Check Homebrew
+if ! type brew > /dev/null 2>&1; then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew's Fomula
+printf "Update Fomula? [Y/n]: " && read ans
+if [ "${ans}" = "Y" ]; then
+    brew update
+fi
+
+# Upgrade Homebrew's Fomula
+printf "Upgrade? [Y/n]: " && read ans
+if [ "${ans}" = "Y" ]; then
+    brew upgrade
+fi
+
+# Tap
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap caskroom/cask
+brew tap caskroom/versions
+brew tap caskroom/fonts
+brew tap dart-lang/dart
+brew tap peco/peco
+brew tap homebrew/apache
+brew tap homebrew/php
+
+# Packages
+packages=(
+
+    # GNU core utilities
+    coreutils
+
+    # Tools
+    grep
+
+    # Shell
+    zsh
+
+    # Language
+    ghc
+    cabal-install
+    rbenv
+    ruby-build
+    elixir
+    dart
+    dartium
+    php54
+    lua
+    python
+
+    # Editor
+    vim --with-lua
+
+    # Multiplexe
+    tmux
+    tmux-mem-cpu-load
+
+    # Git
+    git   
+    tig
+
+    # Utils
+    wget
+    curl
+    bc
+    openssl
+    readline
+    pandoc
+    z
+    the_silver_searcher
+    peco
+)
+
+echo "installing binaries..."
+brew install ${packages[@]} && brew cleanup
+
+# Casks
+brew install brew-cask
+
+apps=(
+    # Browser
+    google-chrome
+    firefox-ja
+    firefoxdeveloperedition-ja
+
+    # Terminal
+    iterm2
+
+    # Editor
+    sublime-text3
+
+    # Communication
+    slack
+    skype
+
+    # VM    
+    virtualbox
+    vagrant
+    vagrant-manager
+
+    # etc
+    karabiner
+    appcleaner
+    dash
+    filezilla
+    onyx
+    skitch
+)
+
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# Fonts
+fonts=(
+    font-ubuntu
+    font-ubuntu-mono-powerline
+    font-clear-sans
+    font-roboto
+    font-noto-sans-japanese
+    font-noto-sans
+    font-noto-serif
+    font-fontawesome
+)
+
+echo "installing fonts..."
+brew cask install ${fonts[@]}
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+# Refer to:
+# https://github.com/tcnksm/dotfiles/blob/master/bootstrap.sh
+
+cd "$(dirname "$0")"
+
+set -e
+
+info() {
+    echo -e "\033[34m$@\033[m" # blue
+}
+
+warn() {
+    echo -e "\033[33m$@\033[m" # yellow
+}
+
+error() {
+    echo -e "\033[31m$@\033[m" # red
+}
+
+info "Install Homebrew Packages"
+./homebrew/Brewfile.sh
+
+info "Setting up prezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+for orrcfile in "$(pwd)"/zsh/prezto-override/*; do
+    ln -sf "$orrcfile" "${ZDOTDIR:-$HOME}/.${orrcfile:t}"
+done
+info "Set Homebrew's Zsh as your default shell:"
+echo "Add homebrew's zsh path to /etc/shells."
+echo "After,"
+echo "$ chsh -s /usr/local/bin/zsh"
+
+info "Enable dotfiles, make symlink to ' ${HOME}' directory"
+ls -si "$(pwd)/tmux/.tmux.conf" "${ZDOTDIR:-$HOME}/.tmux.conf"
+ls -si "$(pwd)/tig/.tigrc" "${ZDOTDIR:-$HOME}/.tigrc"
+
+if [[ ! -d  ~/.tmux/plugins/tmux-powerline ]]; then
+    info "Install tmux-powerline"
+    git clone https://github.com/erikw/tmux-powerline.git ~/.tmux/plugins/tmux-powerline
+fi
