8ebb11b953c8d485671d8d95a8caa0c7fc273b4e
yukiyo <yuki@giamnote.net>
2017-07-26 03:07:39 +0900

    add `update.sh`

--- update.sh
+++ update.sh
@@ -6,17 +6,15 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 	  OS='linux'
 fi
 
-git clone https://github.com/yukink/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+git pull
 
 if [ "$OS" = "mac" ]; then
-    xcode-select --install
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install ansible
+    brew upgrade
     ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/playbook.mac.yml --ask-become-pass
 elif [ "$OS" = "linux" ]; then
     if [ `which apt` ]; then
         sudo apt update
-        sudo apt install ansible
         ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/playbook.ubuntu.yml --ask-become-pass
     fi
 fi
